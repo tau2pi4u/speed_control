42e77164a9ba575223894928c133948a210da919
tau2pi4u <35629184+tau2pi4u@users.noreply.github.com>
2020-09-17 18:37:27 +0100

    Fixed smooth step/max step functionality, added option for per profile transition parameters

--- profile_gen.py
+++ profile_gen.py
@@ -4,24 +4,66 @@ import os
 assert(os.path.isfile('profiles.csv')), "Couldn't find profiles.csv"
 df = pd.read_csv("profiles.csv")
 
+try:
+	df['max_step']
+	max_step = True
+	print("Found max step")
+except:
+	max_step = False
+	print("Didn't find max step")
+	
+try:
+	df['trans_time']
+	trans_time = True
+	print("Found transition time")
+except:
+	trans_time = False
+	print("Didn't find transition time")
+
 profile_count, speed_count = df.shape
 profile_count -= 1
-speed_count -= 1
+speed_count -= (1 + max_step + trans_time)
+
+print(f"Found {profile_count} profiles with {speed_count} speeds")
 
 header = open("speed_control/profiles.h", "w")
 
 header.write(f"#define PROFILE_COUNT {profile_count}\n")
 header.write(f"#define SPEED_COUNT {speed_count}\n")
 
+if trans_time:
+    header.write("#define PER_PROFILE_TRANSITION\n")
+    header.write("#define TRANS_TIME\n")
+
+if max_step:
+    assert(trans_time), "Trying to use max_step without trans time. This is not allowed!"
+    header.write("#define MAX_STEP\n")
+
+
+
+
 for idx, row in df.iterrows():
     assert(idx == row['profile_id']), "Profile ids not in order!"
-    header.write(f"const uint8_t profile_{idx}[SPEED_COUNT] = {{ ")
+    extra_str = ""
+    if trans_time:
+        extra_str += " + 2 /* trans_time */"
+    if max_step:
+        extra_str += " + 1 /* max_step */"
+    header.write(f"const uint8_t profile_{idx}[SPEED_COUNT{extra_str}] = {{ ")
     for speed in range(speed_count):
         voltage = row[str(speed)]
         voltage = min(max(voltage, 0.0), 5.0)
         pwm = int(voltage * 255.0 / 5.0)
         header.write(f"{', ' if speed != 0 else ''}{pwm}")
+    if trans_time:
+	    trans_time_val = row['trans_time']
+	    low_byte = int(trans_time_val) % 256
+	    high_byte = int(trans_time_val / 256) % 256
+	    header.write(f", {low_byte}, {high_byte}")
+    if max_step:
+	    header.write(f", {int(row['max_step'])}")
     header.write("};\n")
+	
 
 header.write("const uint8_t * profile_array[PROFILE_COUNT] = { \n")
 for i in range(profile_count):
--- speed_control/speed_control.ino
+++ speed_control/speed_control.ino
@@ -1,9 +1,20 @@
 #include "pin.h"
 #include "profiles.h"
 
+#if defined(PER_PROFILE_TRANSITION)
 #define SMOOTH_STEP
+#endif
+
+#if !defined(SMOOTH_STEP)
+#define SMOOTH_STEP // comment this out AND remove trans_time from profiles.csv to turn off smooth stepping
+#endif
+#if !defined(TRANS_TIME)
 #define TRANS_TIME 1000
-#define MAX_STEP 2
+#endif
+#if !defined(MAX_STEP)
+#define MAX_STEP 1 // comment this out AND remove max_step from profiles.csv to turn of max step size
+#endif
+
 #define LOOP_DELAY 10
 
 #if defined(SMOOTH_STEP)
@@ -77,6 +88,23 @@ uint8_t get_speed(uint8_t profile_idx, uint8_t speed_idx)
     return profile[SPEED_COUNT-1];
 }
 
+#if defined(PER_PROFILE_TRANSITION)
+uint16_t get_trans_time(uint8_t profile_idx)
+{
+  uint16_t trans_time = get_profile(profile_idx)[SPEED_COUNT+1];
+  trans_time = (trans_time << 8) | get_profile(profile_idx)[SPEED_COUNT];
+  return trans_time;
+}
+
+uint8_t get_max_step(uint8_t profile_idx)
+{
+  return get_profile(profile_idx)[SPEED_COUNT+2];
+}
+#else
+uint16_t get_trans_time(uint8_t profile_idx) { return TRANS_TIME; }
+uint8_t get_max_step(uint8_t profile_idx) { return MAX_STEP; }
+#endif
+
 void set_speed(uint8_t speed)
 {
     analogWrite(PWM_PIN, speed);
@@ -90,7 +118,7 @@ void setup()
 }
 
 #if defined(SMOOTH_STEP) && defined(MAX_STEP)
-uint8_t get_speed(uint8_t target_speed)
+uint8_t get_speed(uint8_t target_speed, uint16_t trans_time, uint8_t max_step)
 {
     if(target_speed != last_target_speed)
     {
@@ -103,19 +131,23 @@ uint8_t get_speed(uint8_t target_speed)
     {
         uint64_t duration = millis() - start_time;
         uint8_t new_speed;
-        if(duration >= TRANS_TIME)
+        if(duration >= trans_time)
         {
             new_speed = target_speed;
         }
+        else if(start_speed < target_speed)
+        {
+            new_speed = map(duration, 0, trans_time, start_speed, target_speed);
+        }
         else
         {
-            new_speed = map(duration, 0, TRANS_TIME, prev_speed, target_speed);
+            new_speed = map(trans_time - duration, 0, trans_time, target_speed, start_speed);
         }
 
-        int8_t step = (int8_t) ((int16_t) prev_speed - (int16_t) new_speed);
-        if(abs(step) > MAX_STEP)
+        int16_t step = ((int16_t) new_speed) - ((int16_t) prev_speed);
+        if(abs(step) > max_step)
         {
-            return step > 0 ? prev_speed + MAX_STEP : prev_speed - MAX_STEP;
+            return step > 0 ? prev_speed + max_step : prev_speed - max_step;
         }
         else
         {
@@ -127,7 +159,7 @@ uint8_t get_speed(uint8_t target_speed)
 #endif
 
 #if defined(SMOOTH_STEP) && !defined(MAX_STEP)
-uint8_t get_speed(uint8_t target_speed)
+uint8_t get_speed(uint8_t target_speed, uint16_t trans_time, uint8_t max_step)
 {
     if(target_speed != last_target_speed)
     {
@@ -140,13 +172,17 @@ uint8_t get_speed(uint8_t target_speed)
     {
         uint64_t duration = millis() - start_time;
         uint8_t new_speed;
-        if(duration >= TRANS_TIME)
+        if(duration >= trans_time)
         {
             new_speed = target_speed;
         }
+        else if(start_speed < target_speed)
+        {
+            new_speed = map(duration, 0, trans_time, start_speed, target_speed);
+        }
         else
         {
-            new_speed = map(duration, 0, TRANS_TIME, prev_speed, target_speed);
+            new_speed = map(trans_time - duration, 0, trans_time, target_speed, start_speed);
         }
 
         return new_speed;
@@ -157,14 +193,14 @@ uint8_t get_speed(uint8_t target_speed)
 
 void loop()
 {
-    static uint64_t loop_start = 0;
+    uint64_t loop_start = millis();
     uint8_t speed_idx = get_speed_idx();
     uint8_t profile_idx = get_profile_idx();
     uint8_t target_speed = get_speed(profile_idx, speed_idx);
 
 // With smooth step it will interpolate between speeds
 #if defined(SMOOTH_STEP)
-    prev_speed = get_speed(target_speed);
+    prev_speed = get_speed(target_speed, get_trans_time(profile_idx), get_max_step(profile_idx));
     set_speed(prev_speed);
 // Without smooth step, it immediately impacts the output
 #else
@@ -175,7 +211,7 @@ void loop()
 // of the output, e.g. with LOOP_DELAY 100 and MAX_STEP 2, that's
 // a max of 20 steps a second which is 392mV/s
 #if defined(SMOOTH_STEP) && LOOP_DELAY > 0
-    loop_duration = millis() - loop_start;
+    uint16_t loop_duration = millis() - loop_start;
     if(loop_duration < LOOP_DELAY)
     {
         delay(LOOP_DELAY - loop_duration);
